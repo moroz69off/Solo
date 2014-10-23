3874603d5e4a38baa06e386ea428645552117f0b
Yuri Morozov <moroz69off@gmail.com>
2014-10-23 20:39:03 +0400

    Solo Speed
    Added core files

---
+++ Solo/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Solo
+{
+    partial class soloForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(soloForm));
+            this.labelSampleText = new System.Windows.Forms.Label();
+            this.textBoxTypedText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelSpeedInfo = new System.Windows.Forms.Label();
+            this.labelTimeInfo = new System.Windows.Forms.Label();
+            this.labelTypeInfo = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buttonChengeText = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // labelSampleText
+            // 
+            this.labelSampleText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelSampleText.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.labelSampleText.Location = new System.Drawing.Point(12, 9);
+            this.labelSampleText.Name = "labelSampleText";
+            this.labelSampleText.Size = new System.Drawing.Size(640, 169);
+            this.labelSampleText.TabIndex = 0;
+            this.labelSampleText.Text = "SampleText";
+            // 
+            // textBoxTypedText
+            // 
+            this.textBoxTypedText.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.textBoxTypedText.Location = new System.Drawing.Point(12, 181);
+            this.textBoxTypedText.Multiline = true;
+            this.textBoxTypedText.Name = "textBoxTypedText";
+            this.textBoxTypedText.Size = new System.Drawing.Size(640, 169);
+            this.textBoxTypedText.TabIndex = 1;
+            this.textBoxTypedText.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.label2.Location = new System.Drawing.Point(12, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Набрано:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.label3.Location = new System.Drawing.Point(193, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Время:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.label4.Location = new System.Drawing.Point(447, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Скорость:";
+            // 
+            // labelSpeedInfo
+            // 
+            this.labelSpeedInfo.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.labelSpeedInfo.Location = new System.Drawing.Point(535, 371);
+            this.labelSpeedInfo.Name = "labelSpeedInfo";
+            this.labelSpeedInfo.Size = new System.Drawing.Size(117, 18);
+            this.labelSpeedInfo.TabIndex = 7;
+            this.labelSpeedInfo.Text = "--";
+            // 
+            // labelTimeInfo
+            // 
+            this.labelTimeInfo.AutoSize = true;
+            this.labelTimeInfo.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.labelTimeInfo.Location = new System.Drawing.Point(271, 371);
+            this.labelTimeInfo.Name = "labelTimeInfo";
+            this.labelTimeInfo.Size = new System.Drawing.Size(18, 18);
+            this.labelTimeInfo.TabIndex = 6;
+            this.labelTimeInfo.Text = "--";
+            // 
+            // labelTypeInfo
+            // 
+            this.labelTypeInfo.AutoSize = true;
+            this.labelTypeInfo.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.labelTypeInfo.Location = new System.Drawing.Point(84, 371);
+            this.labelTypeInfo.Name = "labelTypeInfo";
+            this.labelTypeInfo.Size = new System.Drawing.Size(18, 18);
+            this.labelTypeInfo.TabIndex = 5;
+            this.labelTypeInfo.Text = "--";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // buttonChengeText
+            // 
+            this.buttonChengeText.AutoSize = true;
+            this.buttonChengeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.buttonChengeText.Location = new System.Drawing.Point(27, 418);
+            this.buttonChengeText.Name = "buttonChengeText";
+            this.buttonChengeText.Padding = new System.Windows.Forms.Padding(5);
+            this.buttonChengeText.Size = new System.Drawing.Size(182, 38);
+            this.buttonChengeText.TabIndex = 8;
+            this.buttonChengeText.Text = "Выбрать другой текст";
+            this.buttonChengeText.UseVisualStyleBackColor = true;
+            this.buttonChengeText.Click += new System.EventHandler(this.buttonChengeText_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button1.Location = new System.Drawing.Point(462, 418);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(5);
+            this.button1.Size = new System.Drawing.Size(190, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Выход без завершения";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "Solo";
+            this.openFileDialog.Filter = "text files|*.txt";
+            // 
+            // soloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(664, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonChengeText);
+            this.Controls.Add(this.labelSpeedInfo);
+            this.Controls.Add(this.labelTimeInfo);
+            this.Controls.Add(this.labelTypeInfo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTypedText);
+            this.Controls.Add(this.labelSampleText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "soloForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Соло на клавиатуре";
+            this.Load += new System.EventHandler(this.soloForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSampleText;
+        private System.Windows.Forms.TextBox textBoxTypedText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelSpeedInfo;
+        private System.Windows.Forms.Label labelTimeInfo;
+        private System.Windows.Forms.Label labelTypeInfo;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonChengeText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Solo/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Solo
+{
+    public partial class soloForm : Form
+    {
+        // Переменные========
+        int totalSimbols;
+        int typedSimbols;
+        int speed;
+        DateTime startTime;
+        DateTime currentTime;
+        string messageResultVariant;
+        string textFileName = "Solo.txt";
+        //====================
+
+        public soloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void soloForm_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void init()
+        {
+            try
+            {
+                labelSampleText.Text = File.ReadAllText(textFileName);
+            }
+
+            catch
+            {
+                labelSampleText.Text = "Текст-образец нужно разместить в папке Debug с именем Solo.txt";
+            }
+
+            textBoxTypedText.Text = "";
+            totalSimbols = labelSampleText.Text.Length;
+            typedSimbols = 0;
+            startTime = DateTime.Now;
+            currentTime = DateTime.Now;
+            speed = 0;
+            timer.Enabled = true;
+
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            showStat();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            showStat();
+        }
+
+        private void showStat()
+        {
+            typedSimbols = textBoxTypedText.Text.Length;
+            labelTypeInfo.Text =typedSimbols.ToString() + "/" + totalSimbols.ToString() + "симв.";
+
+
+            bool error;
+            if (typedSimbols > 0)
+            {
+                if (labelSampleText.Text.StartsWith(textBoxTypedText.Text))
+                    error = false;
+                else
+                    error = true;
+            }
+            else
+            {
+                error = false;
+                startTime = DateTime.Now;
+            }
+            textBoxTypedText.BackColor = error ? Color.LightPink : Color.White;
+
+            currentTime = DateTime.Now;
+            double seconds = (currentTime.Ticks - startTime.Ticks) / 10000000.0;
+            labelTimeInfo.Text = seconds.ToString("0.0") + "сек.";
+            if (seconds > 0.1)
+                speed = Convert.ToInt16(typedSimbols * 60 / seconds);
+            else
+                speed = 0;
+            labelSpeedInfo.Text = speed.ToString() + "символов в минуту.";
+
+            if (!error && typedSimbols == totalSimbols)
+                finishGame();
+        }
+
+        //==== Самостоятельная доработка ======================================================================
+        private void messgeresultVariant()
+        {
+            
+            if (speed >= 555) messageResultVariant = " Прими поздравления! Тебе нет равных в скорости набора.";
+                else if  (speed <= 60) messageResultVariant = " Ужасный результат! Попробуй набирать текст хотя бы двумя пальцами.";
+            else messageResultVariant = " Молодец! Хорошая скорость набора.";
+        }
+        //=======================================================================================================
+
+        private void finishGame()
+        {
+            messgeresultVariant();
+            timer.Enabled = false;
+            MessageBox.Show("Набор закончен. Скорость набора: " + labelSpeedInfo.Text + messageResultVariant);  
+            init();
+        }
+
+        //===== Самостоятельная доработка=====================================================================
+        private void buttonChengeText_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = textFileName;  // Здесь я запутался, кнопка должна предложить выбрать другой файл и вставить содержимое файла в мою форму
+        }
+        //==================================================================================================
+
+
+
+
+        
+    }
+}
